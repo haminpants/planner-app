fa9394c26c98800e7263d04c2796ef11f72260e7
mizryl <pham_linda@hotmail.com>
2025-04-06 00:10:23 -0700

    Gradle files configured

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -37,8 +40,16 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.annotation)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    //fragment dependencies
+    val fragment_version = "1.8.6"
+    implementation("androidx.fragment:fragment:$fragment_version")
+
 
 }
