1ef02f1a6763b9ea448ff4f0cb5fae1bc48e2991
mizryl <pham_linda@hotmail.com>
2025-04-05 23:00:46 -0700

    Register redirect works and saving before I make changes to the gradle files to include fragments

--- app/src/main/java/com/info3245/plannerapp/BudgetActivity.java
+++ app/src/main/java/com/info3245/plannerapp/BudgetActivity.java
@@ -1,8 +1,6 @@
 package com.info3245.plannerapp;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class RegisterActivity extends AppCompatActivity {
+public class BudgetActivity extends AppCompatActivity {
 
-    EditText txtFirstName, txtLastName, txtEmailRegister, txtPasswordRegister, txtPasswordConfirmation;
-    Button btnRegister;
     @Override
-    protected void onCreate (Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_budget);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ app/src/main/java/com/info3245/plannerapp/BudgetFragment.java
@@ -0,0 +1,6 @@
+package com.info3245.plannerapp;
+
+import androidx.fragment.app.Fragment;
+
+public class BudgetFragment extends Fragment {
+}
